9867b876ed79d1af12c575fbdcf459e7a1e8590e
AgustinEze <agustineg@gmail.com>
2022-06-19 03:24:00 -0300

    models done

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -7,10 +7,32 @@ module.exports = (sequelize) => {
     id:{
       type: DataTypes.UUID,
       primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUID4
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    hp:{
+      type: DataTypes.INTEGER,
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+    },
+    defense:{
+      type: DataTypes.INTEGER,
+    },
+    speed:{
+      type: DataTypes.INTEGER,
+    },
+    height:{
+      type: DataTypes.INTEGER,
+    },
+    weight:{
+      type: DataTypes.INTEGER
+    }
+  },{
+    timestamp: false
   });
 };
