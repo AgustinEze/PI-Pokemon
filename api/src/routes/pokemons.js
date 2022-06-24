7857ddcf2e9dadf9de0e92d0c36b7a32bd1952df
AgustinEze <agustineg@gmail.com>
2022-06-24 03:03:53 -0300

    back funciona!!!! Esta vivooo!

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -18,12 +18,13 @@ router.post('/', async (req, res, next)=>{
         attack, defense,
         speed, height, weight, image
     });
-    if(type) newPokemon.addTypes(type.map(t=>t*1));
+    console.log(type)
+    type.length? newPokemon.addTypes(type.map(t=>t*1)):newPokemon.addTypes(10001)
     
     res.status(200).send('Creacion OK')
     }catch(err){
         console.log(err)
-        res.status(400).send('Error de creacion')
+        res.status(400).send('Error de creacion') 
     }
 })
 ////////////////////////////////////
@@ -34,21 +35,31 @@ router.get('/',async (req, res, next)=>{
     const {name}=req.query;
     if(name){
         try{
-            let poke =await Pokemon.findOne({where:{name}, include:[Type]})
-            if(poke) return res.send(poke)
-            poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-            console.log('soy pokedata',poke.data)
-            if(poke) return res.send(poke.data)
-
+            let pokeDb =await Pokemon.findOne({where:{name}, include:[Type]})
+            if(pokeDb) return res.send(pokeDb)
+            let pokeApi= await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            return res.send ({
+                id: pokeApi.data.id,
+                name: pokeApi.data.name,
+                image: pokeApi.data.sprites.other.dream_world.front_default,
+                hp: pokeApi.data.stats.filter(st=>st.stat.name==='hp')[0].base_stat,
+                attack: pokeApi.data.stats.filter(st=>st.stat.name==='attack')[0].base_stat,
+                defense: pokeApi.data.stats.filter(st=>st.stat.name==='defense')[0].base_stat,
+                speed: pokeApi.data.stats.filter(st=>st.stat.name==='speed')[0].base_stat,
+                heigth: pokeApi.data.height, 
+                weight: pokeApi.data.weight,
+                type: pokeApi.data.types.map(t=>t.type.name)
+            })
+      
         }catch(err){
             console.log("err")
             return res.status(404).send('No se encontro')
         }    
     }
-
+    //not query
     try{
-        const pokemonDb = await Pokemon.findAll({include:{model: Type}})
-        console.log(pokemonDb)
+        const pokemonDb = await Pokemon.findAll({include:{model: Type},attributes:['id', 'name', 'image', 'attack']})
+
         let pokemonUrl =await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKES}`)
         pokemonUrl = pokemonUrl.data.results.map(poke => poke.url)
         pokemonData = await Promise.all(pokemonUrl.map(async(url)=> await axios.get(url)))
@@ -58,12 +69,12 @@ router.get('/',async (req, res, next)=>{
             id: dataPoke.data.id,
             name: dataPoke.data.name,
             image: dataPoke.data.sprites.other.dream_world.front_default,
-            hp: dataPoke.data.stats.filter(st=>st.stat.name==='hp')[0].base_stat,
+            //hp: dataPoke.data.stats.filter(st=>st.stat.name==='hp')[0].base_stat,
             attack: dataPoke.data.stats.filter(st=>st.stat.name==='attack')[0].base_stat,
-            defense: dataPoke.data.stats.filter(st=>st.stat.name==='defense')[0].base_stat,
-            speed: dataPoke.data.stats.filter(st=>st.stat.name==='speed')[0].base_stat,
-            heigth: dataPoke.data.height, 
-            weight: dataPoke.data.weight,
+            //defense: dataPoke.data.stats.filter(st=>st.stat.name==='defense')[0].base_stat,
+            //speed: dataPoke.data.stats.filter(st=>st.stat.name==='speed')[0].base_stat,
+            //heigth: dataPoke.data.height, 
+            //weight: dataPoke.data.weight,
             type: dataPoke.data.types.map(t=>t.type.name)
         }})
         return res.send([...pokemonDb,...pokemonApi])
@@ -74,9 +85,26 @@ router.get('/',async (req, res, next)=>{
 })
 
 router.get('/:idPokemon',async (req, res, next)=>{
-    const {idPokemon}= req.params;
-    if(idPokemon){
-        return res.send(`soy get pokemons params ${idPokemon}`)
+    let {idPokemon}= req.params;
+    try{
+        const pokeDb = await Pokemon.findByPk(idPokemon, {include:{model: Type}})
+        if(pokeDb) return res.send(pokeDb)
+        const pokeApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+        return res.send ({
+            id: pokeApi.data.id,
+            name: pokeApi.data.name,
+            image: pokeApi.data.sprites.other.dream_world.front_default,
+            hp: pokeApi.data.stats.filter(st=>st.stat.name==='hp')[0].base_stat,
+            attack: pokeApi.data.stats.filter(st=>st.stat.name==='attack')[0].base_stat,
+            defense: pokeApi.data.stats.filter(st=>st.stat.name==='defense')[0].base_stat,
+            speed: pokeApi.data.stats.filter(st=>st.stat.name==='speed')[0].base_stat,
+            heigth: pokeApi.data.height, 
+            weight: pokeApi.data.weight,
+            type: pokeApi.data.types.map(t=>t.type.name)
+        }) 
+    }catch(err){
+        console.log(err)
+        return res.status(404).send('No se encontro')
     }
 })
 
