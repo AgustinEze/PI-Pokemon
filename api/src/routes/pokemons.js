8e82f1d8e1dd0f99ea546eed8e858daf0d196a9a
AgustinEze <agustineg@gmail.com>
2022-08-31 18:19:48 -0300

    carga desde json

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,21 +1,17 @@
-const { default: axios } = require('axios');
+const { default: axios } = require('axios'); 
 const { Router } = require('express');
 const {Pokemon, Type} = require('../db');
-
+const pokemons = require('./pokes.json')
 const router = Router();
 
-const MAX_POKES = 10
+const MAX_POKES = 100
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+
 router.post('/', async (req, res, next)=>{ 
     let {image, name, hp, type, 
         attack, defense, speed, height, 
         weight}=req.body
 
-/*     if(!image){
-        image=__dirname+'/images/pokeDefault.jpg'
-    } */
     try{const newPokemon = await Pokemon.create(
         {name, hp,
         attack, defense,
@@ -38,24 +34,17 @@ router.get('/',async (req, res, next)=>{
     const {name}=req.query;
     if(name){
         try{
-            let pokeDb =await Pokemon.findOne({where:{name}, include:[Type]})
+            let pokeDb = await Pokemon.findOne({where:{name}, include:[Type]})
             if(pokeDb) return res.send([pokeDb])
-            let pokeApi= await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            let pokeApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
             return res.send ([{
                 id: pokeApi.data.id,
                 name: pokeApi.data.name,
                 image: pokeApi.data.sprites.other.dream_world.front_default,
-                //hp: pokeApi.data.stats.filter(st=>st.stat.name==='hp')[0].base_stat,
-                //attack: pokeApi.data.stats.filter(st=>st.stat.name==='attack')[0].base_stat,
-                //defense: pokeApi.data.stats.filter(st=>st.stat.name==='defense')[0].base_stat,
-                //speed: pokeApi.data.stats.filter(st=>st.stat.name==='speed')[0].base_stat,
-                //heigth: pokeApi.data.height, 
-                //weight: pokeApi.data.weight,
                 type: pokeApi.data.types.map(t=>t.type.name)
             }])
-      
         }catch(err){
-            console.log("err")
+            console.log(err)
             return res.status(404).send('No se encontro')
         }    
     }
@@ -65,7 +54,7 @@ router.get('/',async (req, res, next)=>{
             {include: {model:Type},
             attributes:['id', 'name', 'image', 'attack']})
 
-        let pokemonUrl =await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKES}`)
+        /* let pokemonUrl =await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKES}`)
         pokemonUrl = pokemonUrl.data.results.map(poke => poke.url)
         pokemonData = await Promise.all(pokemonUrl.map(async(url)=> await axios.get(url)))
  
@@ -83,6 +72,8 @@ router.get('/',async (req, res, next)=>{
             type: dataPoke.data.types.map(t=>t.type.name)
         }})
         return res.send([...pokemonDb,...pokemonApi])
+         */
+        return res.send([...pokemonDb,...pokemons])
     }catch(err){
         console.log(err)
         res.status(400).send('Error')
@@ -92,9 +83,11 @@ router.get('/',async (req, res, next)=>{
 router.get('/:idPokemon',async (req, res, next)=>{
     let {idPokemon}= req.params;
     try{
-        const pokeDb = await Pokemon.findByPk(idPokemon, /* {include:{model: Type} }*/)
+        if(idPokemon.length>5){
+        const pokeDb = await Pokemon.findByPk(idPokemon, {include:{model: Type} })
+        console.log('esto es pokeDB',pokeDb)
         if(pokeDb) return res.send(pokeDb)
-        const pokeApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+        }const pokeApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
         return res.send ({
             id: pokeApi.data.id,
             name: pokeApi.data.name,
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import {Home} from './components/Home'
-import Pokemons from './components/Pokemons'
+import Pokemons from './components/pokemons.jsx'
 import {NewPokemon} from './components/NewPokemon'
 import {PokemonDetails} from './components/PokemonDetails'
-
 import {Route} from 'react-router-dom'
+import { useEffect } from 'react';
+
+
 function App() {
+  useEffect(()=>{
+    
+  },[])
   return (
     <div className="App">
       <Route exact path='/pokemons/:id' component={PokemonDetails}/>
